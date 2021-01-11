963cbcea63fbf254ce4613aba26e9c2441d07346
JeffVi <57104907+JeffVi@users.noreply.github.com>
2021-01-11 17:09:23 +0100

    Update similarity_graph.cpp

--- src/similarity_graph.cpp
+++ src/similarity_graph.cpp
@@ -85,3 +85,104 @@ void similarity_graph::init_graph()
 	pixels_nodes.at<int>(rows-1,cols-1,6) = 1;
 	
 }
+
+void similarity_graph::compare_YUV()
+{
+	bool dY;
+	bool dU;
+	bool dV;
+	
+	int vi;
+	int vj; 
+	
+	for(int i=0;i<rows;i++)
+	{
+		for(int j=0;j<cols;j++)
+		{
+			for(int k=0;k<8;k++)
+			{
+				if(pixels_nodes.at<int>(i,j,k) == 1)
+				{
+					int Y = image_YUV.at<Vec3b>(i,j)[0];
+					int U = image_YUV.at<Vec3b>(i,j)[1];
+					int V = image_YUV.at<Vec3b>(i,j)[2];
+			
+					auto voisin = get_voisin(i,j,k);
+					int vi = std::get<0>(voisin);
+					int vj = std::get<1>(voisin);
+					int vk = std::get<2>(voisin);
+					
+					dY = abs(Y - image_YUV.at<Vec3b>(vi,vj)[0])>48;
+					dU = abs(U - image_YUV.at<Vec3b>(vi,vj)[1])>7;
+					dV = abs(V - image_YUV.at<Vec3b>(vi,vj)[2])>6;
+					
+					if(dY || dU || dV)
+					{
+						pixels_nodes.at<int>(i,j,k) = 0;
+						pixels_nodes.at<int>(vi,vj,vk) = 0;
+					}
+				}
+			}
+		}
+	}
+}
+
+std::tuple<int, int, int> similarity_graph::get_voisin(int i, int j, int k)
+{
+	int vi;
+	int vj;
+	int vk;
+	
+	switch(k)
+	{
+		case 0:
+		vi = i-1;
+		vj = j;
+		vk = 4;
+		break;
+		
+		case 1:
+		vi = i-1;
+		vj = j+1;
+		vk = 5;
+		break;
+		
+		case 2:
+		vi = i;
+		vj = j+1;
+		vk = 6;
+		break;
+		
+		case 3:
+		vi = i+1;
+		vj = j+1;
+		vk = 7;
+		break;
+		
+		case 4:
+		vi = i+1;
+		vj = j;
+		vk = 0;
+		break;
+		
+		case 5:
+		vi = i+1;
+		vj = j-1;
+		vk = 1;
+		break;
+		
+		case 6:
+		vi = i;
+		vj = j-1;
+		vk = 2;
+		break;
+		
+		case 7:
+		vi = i-1;
+		vj = j-1;
+		vk = 3;
+		break;
+	}
+	
+	return std::make_tuple(vi,vj,vk);
+}
