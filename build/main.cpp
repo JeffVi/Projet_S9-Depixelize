41f29667a5f2b9a116a84ea65bc9ba0100fe48bb
JeffVi <57104907+JeffVi@users.noreply.github.com>
2021-01-11 11:42:47 +0100

    Update main.cpp

--- build/main.cpp
+++ build/main.cpp
@@ -15,7 +15,12 @@ int main()
     }
     namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", image);
-
+    
+    int nbr_lignes = image.rows;
+    int nbr_colonnes = image.cols;
+    
+    std::cout<<"lignes : "<<nbr_lignes<<" colonnes : "<<nbr_colonnes<<std::endl;
+    
     waitKey(0);
 
     return 0;
