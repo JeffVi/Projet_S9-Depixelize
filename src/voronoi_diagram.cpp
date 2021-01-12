1b4be745d47137f744925b09f423eb39fb8cd4c4
JeffVi <57104907+JeffVi@users.noreply.github.com>
2021-01-12 12:28:13 +0100

    Update voronoi_diagram.cpp

--- src/voronoi_diagram.cpp
+++ src/voronoi_diagram.cpp
@@ -14,3 +14,19 @@ voronoi::voronoi(Mat image ,Mat pixels_nodes, float diagram_scale)
 	pixels_nodes_todo = pixels_nodes;
 	scale = diagram_scale;
 }
+
+void voronoi::init_cells()
+{
+	for(int i=0;i<rows;i++)
+	{
+		for(int j=0;j<cols;j++)
+		{
+			cell c(i*scale + scale/2 , j*scale + scale/2);
+			c.vertex.push_back(Point(j*scale,i*scale)); 		// haut gauche
+			c.vertex.push_back(Point((j+1)*scale,i*scale)); 	// haut droite
+			c.vertex.push_back(Point((j+1)*scale,(i+1)*scale)); 	// bas droite
+			c.vertex.push_back(Point(j*scale,(i+1)*scale)); 	// bas gauche
+			cells.push_back(c);
+		}
+	}
+}
