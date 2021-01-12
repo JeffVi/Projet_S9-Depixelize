b6a06015fd0590560d02176639c74b3fd083ef35
JeffVi <57104907+JeffVi@users.noreply.github.com>
2021-01-12 11:51:13 +0100

    Update voronoi_diagram.h

--- src/voronoi_diagram.h
+++ src/voronoi_diagram.h
@@ -3,4 +3,43 @@
 
 #include <opencv2/opencv.hpp>
 
+using namespace cv;
+
+class cell
+{
+public:
+	
+	cell();
+	cell(float x, float y);
+	
+	float px;
+	float py;
+	
+private:
+	
+	std::vector<Point> vertex;
+	
+};
+
+class voronoi
+{
+public:
+
+	voronoi();
+	voronoi(Mat image ,Mat pixels_nodes);
+	
+	void init_cells();
+	void compute_vertex();
+	
+	
+private:
+	
+	int rows;
+	int cols;
+	Mat imagep;
+	Mat pixels_nodes_todo;
+	std::vector<cell> cells;
+
+};
+
 #endif
