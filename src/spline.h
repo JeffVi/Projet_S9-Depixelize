3df7f589ba9bf76072234a1340c2de3c2d11476d
JeffVi <57104907+JeffVi@users.noreply.github.com>
2021-01-18 09:20:18 +0100

    Update spline.h

--- src/spline.h
+++ src/spline.h
@@ -2,6 +2,7 @@
 #define SPLINE_H
 
 #include <opencv2/opencv.hpp>
+#include "voronoi_diagram.h"
 
 using namespace cv;
 
@@ -15,7 +16,6 @@ public:
 	
 };
 
-
 class spline
 {
 public:
@@ -23,18 +23,33 @@ public:
 	spline();
 	spline(std::vector<Point> vertex_list);
 	
-	std::vector<Point> vertex;
 	std::vector<polygon> holes;
 	polygon spline_poly;
 	
 	void find_holes();
+	Mat draw_spline(float scale, int rows, int cols);
 	
 private:
 
 	std::vector<Point>::iterator it_p1;
 	std::vector<Point>::iterator it_p2;
+	std::vector<Point> vertex;
 	
-	bool find_edge(std::vector<Point>::iterator it_1, std::vector<Point>::iterator it_2);
+	bool find_edge(std::vector<Point>::iterator& it_1, std::vector<Point>::iterator& it_2);
+};
+
+class B_splines
+{
+public:
+	
+	B_splines();
+	B_splines(voronoi& voronoi_diagram);
+	
+	std::vector<spline> splines;
+	
+	void find_holes_splines();
+	void print_spline(int id_spline);
+	
 };
 
 #endif
